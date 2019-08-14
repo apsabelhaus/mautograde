0098a4a86e7e582383c106b3ecedfcb77c4bfe90
Roberto Tron <tron@MacBook-Pro.local>
2019-08-14 16:07:37 -0400

    Fixed a bug due to different implementation of regexp(...,'match') on Octave/Linux

--- mautogradeFunctionScan.m
+++ mautogradeFunctionScan.m
@@ -1,4 +1,4 @@
-function functionInfo=mAutogradeFunctionScan(fileName)
+function functionInfo=mautogradeFunctionScan(fileName)
 fid=fopen(fileName);
 if fid<0
     error(['File ' fileName ' not found'])
@@ -11,7 +11,7 @@ while ~feof(fid)
     line1=fgetl(fid);
     %Check if line defines a function, and capture name if so
     tokens1=regexp(line1,'^\s*function\s*(?<argoutWithEqual>\w*\s*=\s*)*(?<fname>\w*)','names');
-    if ~isempty(tokens1)
+    if ~isempty(tokens1) && ~isempty(tokens1.fname)
         line2=fgetl(fid);
         %Check if the next line is metadata, and capture it if so
         tokens2=regexp(line2,'^\s*%\s*MAX_SCORE\s*=*\s*(?<maxScore>[\d\.]+)','names');
