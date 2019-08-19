fd6daa61073b19bd999159d4503cea8ba4d310ab
Roberto Tron <tron@bu.edu>
2019-08-19 15:59:18 -0400

    Added verbose option to SuiteScan

--- mautogradeSuiteScan.m
+++ mautogradeSuiteScan.m
@@ -1,4 +1,18 @@
-function functionInfo=mautogradeSuiteScan(fileName)
+function functionInfo=mautogradeSuiteScan(fileName,varargin)
+flagVerbose=false;
+
+%optional parameters
+ivarargin=1;
+while ivarargin<=length(varargin)
+    switch lower(varargin{ivarargin})
+        case 'verbose'
+            flagVerbose=true;
+        otherwise
+            error(['Argument ' varargin{ivarargin} ' not valid!'])
+    end
+    ivarargin=ivarargin+1;
+end
+
 fid=fopen(fileName);
 if fid<0
     error(['File ' fileName ' not found'])
@@ -9,6 +23,9 @@ lineParserState='searchingFunction';
 
 while ~feof(fid)
     line=fgetl(fid);
+    if flagVerbose
+        disp(['state: ' lineParserState '; line: ' line])
+    end
     switch lineParserState
         case 'searchingFunction'
             %Check if line defines a function, and capture name if so
@@ -16,6 +33,9 @@ while ~feof(fid)
             if ~isempty(tokens) && ~isempty(tokens.fname)
                 currentFunctionName=tokens.fname;
                 lineParserState='searchingMetadata';
+                if flagVerbose
+                    fprintf('Entered function %s, transition to %s\n', currentFunctionName, lineParserState);
+                end
             end
         case 'searchingMetadata'
             %Start by setting default: if no metadata is found, we go back
@@ -28,6 +48,9 @@ while ~feof(fid)
                 name=mautogradeFunctionNameJoin(fileName,currentFunctionName);
                 functionInfo.(name).score=str2double(tokens.maxScore);
                 lineParserState='searchingMetadata';
+                if flagVerbose
+                    fprintf('Found option MAX_SCORE = %d\n', functionInfo.(name).score);
+                end
             end
             %VISIBILITY
             tokens=regexp(line,'^\s*%\s*VISIBILITY\s*=*\s*(?<visibility>[\w_]+)','names');
@@ -35,6 +58,12 @@ while ~feof(fid)
                 name=mautogradeFunctionNameJoin(fileName,currentFunctionName);
                 functionInfo.(name).visibility=tokens.visibility;
                 lineParserState='searchingMetadata';
+                if flagVerbose
+                    fprintf('Found option VISIBILITY = %s\n', functionInfo.(name).visibility);
+                end
+            end
+            if flagVerbose && strcmp(lineParserState,'searchingFunction')
+                fprintf('Found non-option line, transitioning to %s\n', lineParserState);
             end
     end
 end
