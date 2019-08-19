8f11efaeed9488d9e21bbe20dbe7df46d5e0bacd
Roberto Tron <tron@bu.edu>
2019-08-19 16:12:48 -0400

    Changed newline character for output.

--- mautogradeAppendOutput.m
+++ mautogradeAppendOutput.m
@@ -1,9 +1,10 @@
 %function outputMsg=mAutogradeAppendOutput(outputMsg,varargin)
 function outputMsg=mautogradeAppendOutput(outputMsg,varargin)
+newLineStr=newline;
 if ~ischar(outputMsg)
     outputMsg=num2str(outputMsg);
 end
 if ~isempty(outputMsg)
-    outputMsg=[outputMsg ' <br/> '];
+    outputMsg=[outputMsg newLineStr];
 end
 outputMsg=sprintf([outputMsg varargin{1}],varargin{2:end});
