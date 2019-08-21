af1df9795613fbec756e4f135b36716d28dc939c
Roberto Tron <tron@bu.edu>
2019-08-21 13:51:07 -0400

    Now Any2Str can handle arrays of structs.

--- mautogradeAny2Str.m
+++ mautogradeAny2Str.m
@@ -34,17 +34,24 @@ switch class(s)
         str=mautogradeChar2Multiline(str);
         str=mautogradeAppendOutput(strPrev,str);
     case 'struct'
-        sNamesCells=['struct' ...
-            reshape([fieldnames(s) struct2cell(s)]',1,[])...
-            'endstruct'];
-        str=mautogradeAny2Str(sNamesCells,'appendTo',strPrev);
-    case 'cell'
         str=strPrev;
-        s=reshape(s,1,[]);
-        for iCell=1:length(s)
-            str=mautogradeAny2Str(s{iCell},'appendTo',str);
+        for iStruct=1:numel(s)
+            thisStruct=s(iStruct);
+            sNamesCells=[fieldnames(thisStruct) struct2cell(thisStruct)];
+            str=mautogradeAny2Str(sNamesCells,'appendTo',str);
         end
-        
+        str=mautogradeAppendOutput('struct',str);
+        str=mautogradeAppendOutput(str,'endstruct');
+    case 'cell'
+        cellStr=cellfun(@mautogradeAny2Str,s,'UniformOutput',false);
+        strConcatenation=[];
+        nbRows=size(cellStr,1);
+        for iCol=1:size(cellStr,2)
+            strConcatenation=[strConcatenation repmat(' ',nbRows,1)...
+                char(cellStr(:,iCol))];
+        end
+        strConcatenation=mautogradeChar2Multiline(strConcatenation);
+        str=mautogradeAppendOutput(strPrev,strConcatenation);        
     case 'logical'
         sStrCell=arrayfun(@logical2str, s, 'UniformOutput',false);
         str=mautogradeAny2Str(sStrCell,'appendTo',strPrev);
