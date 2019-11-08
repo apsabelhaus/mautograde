f20ae7f261e10d41234edeafd6d818669495015e
Roberto Tron <tron@bu.edu>
2019-11-08 17:45:39 -0500

    Utility to run regexrep on files

---
+++ mautogradeRegexpReplaceInFile.m
@@ -0,0 +1,31 @@
+%Run regexprep on a file instead of a string
+%function nbMatches=mautogradeRegexpReplaceInFile(fileName, varargin)
+%Inputs
+%   fileName    name of the file on which to perform the search and replace
+%   varargin    these arguments are passed to regexprep
+%Outputs
+%   nbMatches   total number of matches in the file for the given expression
+function nbMatches=mautogradeRegexpReplaceInFile(fileName, varargin)
+flagCountMatches=nargout>0;
+if ~exist(fileName, 'file')
+    error('mautogradeRegexpReplaceInFile:no_file', 'File not found');
+end
+
+% Read the entire file with lines in a cell array
+fid=fopen(fileName); 
+text=textscan(fid,'%s','Delimiter','');
+text=text{1};
+fclose(fid);
+
+% Count number of matches (substitutions that will be done), if necessary
+if flagCountMatches
+    nbMatches=sum(cellfun(@length,regexp(text,varargin{1})));
+end
+
+% Find and replace.
+text = regexprep(text, varargin{:});
+
+% Write out the new file.
+fid = fopen(fileName, 'w');
+fprintf(fid,['%s' newline()],text{:});
+fclose(fid);
