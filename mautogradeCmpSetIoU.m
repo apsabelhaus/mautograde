b69bba7e37e02e7f242afc2360d070a22b8c73dc
Roberto Tron <tron@bu.edu>
2019-11-08 17:45:54 -0500

    Comparison function for sets (IoU metric)

---
+++ mautogradeCmpSetIoU.m
@@ -0,0 +1,49 @@
+%Compare two arrays or cell arrays regardless of ordering
+%function [fractionCorrect,totalItems]=mautogradeCmpEq(expected, actual, varargin)
+%Returns the IoU (Intersection over Union) metric for the two input
+%collections. This function works directly only on the collections supported by the
+%functions union and intersection; otherwise, each element of the
+%collection will be attempted to be converted to a string (using
+%mautogradeAny2Str) before comparison.
+%Inputs
+%   expected, actual: arrays or cell arrays to compare
+%Outputs
+%   fractionCorrect     fraction (between 0 and 1) of how many element coincide
+%   totalItems          total numbe of items in the expected collection
+function [fractionCorrect,totalItems]=mautogradeCmpSetIoU(expected, actual, varargin)
+flagRawCounts=false;
+
+%optional parameters
+ivarargin=1;
+while ivarargin<=length(varargin)
+    switch lower(varargin{ivarargin})
+        case 'rawcounts'
+            flagRawCounts=true;
+        otherwise
+            error(['Argument ' varargin{ivarargin} ' not valid!'])
+    end
+    ivarargin=ivarargin+1;
+end
+
+if ~isCellTypeCompatible(expected) || ~isCellTypeCompatible(actual)
+    if isa(expected,'cell') || isa(actual,'cell')
+        expected=cellfun(@mautogradeAny2Str,mautogradeEnsureCell(expected),'UniformOutput',false);
+        actual=cellfun(@mautogradeAny2Str,mautogradeEnsureCell(actual),'UniformOutput',false);
+    elseif isa(expected,'struct') && isa(actual,'struct')
+        %two struct arrays, compare by string representations
+        expected=arrayfun(@mautogradeAny2Str,expected,'UniformOutput',false);
+        actual=arrayfun(@mautogradeAny2Str,actual,'UniformOutput',false);
+    else
+        error('Types not supported')
+    end
+end
+
+totalItems=length(expected);
+fractionCorrect=length(intersect(expected,actual))/length(union(expected,actual));
+
+if flagRawCounts
+    fractionCorrect=fractionCorrect*totalItems;
+end
+
+function flag=isCellTypeCompatible(c)
+flag=isnumeric(c) || all(cellfun(@isnumeric,c)) || all(cellfun(@ischar,c));
