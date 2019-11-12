0068517d244a7b51594292ec2bd78232eb4ce7a0
Roberto Tron <tron@bu.edu>
2019-11-12 14:12:00 -0500

    Added case of empty argument to Char2Multiline

--- mautogradeChar2Multiline.m
+++ mautogradeChar2Multiline.m
@@ -1,9 +1,12 @@
 function str=mautogradeChar2Multiline(charArray)
 nbStrings=size(charArray,1);
-if nbStrings==1
-    str=charArray;
-else
-    str=reshape([charArray repmat(char(10),nbStrings,1)]',1,[]); %#ok<CHARTEN>
-    %remove trailing newline
-    str(end)=[];
+switch nbStrings
+    case 0
+        str='';
+    case 1
+        str=charArray;
+    otherwise
+        str=reshape([charArray repmat(char(10),nbStrings,1)]',1,[]); %#ok<CHARTEN>
+        %remove trailing newline
+        str(end)=[];
 end
