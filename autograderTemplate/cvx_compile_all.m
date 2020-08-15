3dc71d312e5f42a97c55cf0739c08d64e94ea525
Roberto Tron <tron@bu.edu>
2020-08-15 07:27:47 -0400

    Added automatic compilation of CVX

--- autograderTemplate/cvx_compile_all.m
+++ autograderTemplate/cvx_compile_all.m
@@ -3,3 +3,5 @@ cd ../sdpt3
 install_sdpt3 -nopath
 cd ../sedumi
 install_sedumi -nopath
+cd ..
+cvx_setup
--- autograderTemplate/setup_cvx.sh
+++ autograderTemplate/setup_cvx.sh
@@ -2,8 +2,10 @@
 CVX_DIR="${AUTOGRADER_DIR}/toolbox/cvx"
 mkdir -p ${CVX_DIR}
 
+# Get libraries necessaries for compilation
 apt-get --yes install libopenblas-dev liboctave-dev
 
+# Get CVX and solvers
 git clone -b rework https://github.com/cvxr/CVX.git/ ${CVX_DIR}
 cd ${CVX_DIR}
 rm -rf sdpt3/
@@ -11,3 +13,6 @@ rm -rf sedumi/
 git clone https://github.com/sqlp/sdpt3.git
 git clone https://github.com/sqlp/sedumi.git
 cp "${AUTOGRADER_DIR_SOURCE}/cvx_compile_all.m" .
+
+# Compile CVX under octave
+octave --no-gui --eval "cvx_compile_all"
